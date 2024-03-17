c348addc2a9c41eb25a274e3cfbc530c75650bd1
Marlea <marleamartens1@gmail.com>
2024-03-16 20:54:58 -0400

    Fixed some stuff

---
+++ swatrideshare/firebaseConfig.js
@@ -0,0 +1,22 @@
+      //start of firebase stuff
+      // Import the functions you need from the SDKs you need
+      import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+      import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+      // TODO: Add SDKs for Firebase products that you want to use
+      // https://firebase.google.com/docs/web/setup#available-libraries
+    
+      // Your web app's Firebase configuration
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      const firebaseConfig = {
+        apiKey: "AIzaSyBjjfh5U8b37ABG_yKTO2dYmeIdrZZhKCw",
+        authDomain: "swatrideshare.firebaseapp.com",
+        projectId: "swatrideshare",
+        storageBucket: "swatrideshare.appspot.com",
+        messagingSenderId: "1029074874816",
+        appId: "1:1029074874816:web:ce8fbc5b49af9890af9825",
+        measurementId: "G-3RN66MB1JF"
+      };
+    
+      // Initialize Firebase
+      const app = initializeApp(firebaseConfig);
+      const analytics = getAnalytics(app);
